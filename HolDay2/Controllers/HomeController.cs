92cfcf044e10d0c7ab83ae877875ef2dc981d506
Abhishek Kumar <hansrajsharma8252@gmail.com>
2022-11-24 16:08:43 +0530

    Add project files.

---
+++ HolDay2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using HolDay2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HolDay2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return Content("<h1>This is a demo of  ContentResult </ h1 > ");
+            }
+            else if (id.ToLower() == "plain")
+
+            {
+                return Content("<h1>This is a demo of  ContentResult </ h1 > ", "text / plain");
+            }
+            else if (id.ToLower() == "html")
+            {
+                return Content("<h1>This is a demo of   ContentResult </ h1 > ", "text / html");
+            }
+            else if (id.ToLower() == "xml")
+            {
+                return Content("<h1>This is a demo   ContentResult </ h1 > ", "text / xml");
+            }
+            return Content("Invalid content type");
+        }
+        public ActionResult DownloadFile()
+        {
+            if (!System.IO.File.Exists(@"C:\Users\lighthouse.jpg"))
+            {
+                return Content("File to be downloaded not found");
+            }
+            return File(@"C:\Users\lighthouse.jpg", "image /jpg", "default.jpg");
+        }
+        public ActionResult Google()
+        {
+            return Redirect("http://www.google.com");
+        }
+        public ActionResult StartPage()
+        {
+            return RedirectToAction("Index", new { id = "xml" });
+        }
+        public ActionResult Login() { return View(); }
+        public ActionResult AboutUs() { return View(); }
+    }
+}
---
+++ HolDay2/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HolDay2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            //pass a list of products to the view
+            return View(p.GetProducts());
+            
+        }
+    }
+}
